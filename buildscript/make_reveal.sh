dd96391ed518a4ca5d167b959279b5caa5e8f88c
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-04-03 21:22:58 +0100

    fix bug in _reveal.xml files

--- buildscript/make_reveal.sh
+++ buildscript/make_reveal.sh
@@ -4,6 +4,6 @@ mkdir -p tmp
 
 filename=$(basename "$1")
 newfile="${filename%.*}_reveal.xml"
-shortname="${filename%.*}"
+shortname="${filename%.*}_hidden"
 
 cat reveal.xml | sed -e "s/REPLACEME/$shortname/" > tmp/$newfile
