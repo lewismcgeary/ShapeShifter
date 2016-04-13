5c3f3c4e1d910145ebb5f4abef6d90bb16eb3568
Lewis McGeary <lewismcgeary@gmail.com>
2016-04-13 15:13:36 +0100

    change target sdk to 22, no access to api23 device, emulator won't give access to microphone

--- app/src/main/java/io/github/lewismcgeary/shapeshifter/MainActivity.java
+++ app/src/main/java/io/github/lewismcgeary/shapeshifter/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements VoiceInputResults
     private ImageView emptyMicImageView;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
