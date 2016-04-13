fe160d54cd3eb037b6c405d504b211c8a919a07f
Lewis McGeary <lewismcgeary@gmail.com>
2016-04-13 02:33:53 +0100

    add animation to introActivity and click to move on

--- app/src/main/java/io/github/lewismcgeary/shapeshifter/IntroActivity.java
+++ app/src/main/java/io/github/lewismcgeary/shapeshifter/IntroActivity.java
@@ -1,7 +1,11 @@
 package io.github.lewismcgeary.shapeshifter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        ImageView logoImageView = (ImageView)findViewById(R.id.logo_image_view);
+        AnimatedVectorDrawableCompat logoZoom = AnimatedVectorDrawableCompat.create(this, R.drawable.logo_zoom);
+        logoImageView.setImageDrawable(logoZoom);
+        logoZoom.start();
+        logoImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startShapeShifterActivity();
+            }
+        });
+    }
+
+    private void startShapeShifterActivity(){
+        Intent newActivity = new Intent(this, MainActivity.class);
+        startActivity(newActivity);
     }
 }
